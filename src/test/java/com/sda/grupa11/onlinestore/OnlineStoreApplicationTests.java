0b2577a0e7bd0167b2c87b58ec9018f579c0ab5b
Vladinho1990 <vldburlacu@yahoo.com>
2020-02-02 10:11:44 +0200

    --create project--

---
+++ src/test/java/com/sda/grupa11/onlinestore/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sda.grupa11.onlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
