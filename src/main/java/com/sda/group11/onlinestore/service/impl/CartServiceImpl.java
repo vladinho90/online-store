d12f71542c959d223a529ddcbba8b6d96107d091
Emanuel Ciuca <emanuel.ciuca@gmail.com>
2020-02-15 22:04:38 +0200

    Fix transactional issues and update checkout method

--- src/main/java/com/sda/group11/onlinestore/service/impl/CartServiceImpl.java
+++ src/main/java/com/sda/group11/onlinestore/service/impl/CartServiceImpl.java
@@ -5,6 +5,7 @@ import com.sda.group11.onlinestore.model.*;
 import com.sda.group11.onlinestore.model.enums.Status;
 import com.sda.group11.onlinestore.repository.CartItemRepository;
 import com.sda.group11.onlinestore.repository.CartRepository;
+import com.sda.group11.onlinestore.repository.OrderRepository;
 import com.sda.group11.onlinestore.service.ICartService;
 import com.sda.group11.onlinestore.service.IOrderService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 @Transactional
 @Service
 public class CartServiceImpl implements ICartService {
@@ -35,19 +37,22 @@ public class CartServiceImpl implements ICartService {
     @Autowired
     public ProductServiceImpl productService;
 
+    @Autowired
+    public OrderRepository orderRepository;
+
     @Override
     public Cart getCart(User user) {
         return user.getCart();
     }
 
     @Override
-    public void delete(Long cartItemId , User user){
-       Optional<CartItem> cartItem= getCart(user).getCartItemSet()
-               .stream().filter(item-> cartItemId.equals(item.getProduct().getId())).findFirst();
-       cartItem.ifPresent(productInOrder -> {
-           productInOrder.setCart(null);
-           cartRepository.deleteById(productInOrder.getId());
-       });
+    public void delete(Long cartItemId, User user) {
+        Optional<CartItem> cartItem = getCart(user).getCartItemSet()
+                .stream().filter(item -> cartItemId.equals(item.getProduct().getId())).findFirst();
+        cartItem.ifPresent(productInOrder -> {
+            productInOrder.setCart(null);
+            cartRepository.deleteById(productInOrder.getId());
+        });
     }
 
     @Override
@@ -63,20 +68,20 @@ public class CartServiceImpl implements ICartService {
 
     @Override
     public Cart findCartById(Long id) {
-        return cartRepository.findById(id).orElseThrow(()-> new RuntimeException("cart with id "+ id +" not found"));
+        return cartRepository.findById(id).orElseThrow(() -> new RuntimeException("cart with id " + id + " not found"));
     }
 
     @Override
-    public void addToCart(CartItem cartItem, User user){
+    public void addToCart(CartItem cartItem, User user) {
         Cart finalCart = user.getCart();
         Set<CartItem> set = finalCart.getCartItemSet();
         Optional<CartItem> old = set.stream().filter(item -> item.getProduct().getId().equals(cartItem.getProduct().getId())).findFirst();
         CartItem item;
-        if(old.isPresent()){
-            item=old.get();
-            item.setQuantity(cartItem.getQuantity()+item.getQuantity());
-        }else{
-            item= cartItem;
+        if (old.isPresent()) {
+            item = old.get();
+            item.setQuantity(cartItem.getQuantity() + item.getQuantity());
+        } else {
+            item = cartItem;
             item.setCart(finalCart);
             finalCart.getCartItemSet().add(item);
         }
@@ -88,34 +93,46 @@ public class CartServiceImpl implements ICartService {
     @Override
     public void deleteCart(Long cartItemId, User user) {
         Optional<CartItem> cartItemOptional = user.getCart().getCartItemSet().stream().filter(itemId -> cartItemId.equals(itemId.getId())).findFirst();
-        cartItemOptional.ifPresent(cartItem ->{
+        cartItemOptional.ifPresent(cartItem -> {
             cartItem.setCart(null);
             cartItemRepository.deleteById(cartItem.getId());
         });
     }
 
     @Override
-    public List<OrderLine> checkout(Long cartId){
-        Order order= new Order();
-        BigDecimal orderPrice= BigDecimal.valueOf(0);
+    public List<OrderLine> checkout(Long cartId) {
+        Order order = new Order();
         order.setStatus(Status.NEW);
-        List<OrderLine> orderLineList=new ArrayList<>();
+
         Cart cart = findCartById(cartId);
-        cart.getCartItemSet().forEach(cartItem -> {
-            OrderLine orderLine=new OrderLine();
-            orderLine.setProduct(cartItem.getProduct());
-            orderLine.setOrder(order);
-            orderLine.setPrice(cartItem.getProduct().getPrice());
-            orderLine.setQuantity(cartItem.getQuantity());
-            cartItem.setCart(null);
 
-            //trebuie sa facem o metoda care sa scada unitatile din stoc
-            productService.decreaseStock(cartItem.getProduct().getId(),cartItem.getQuantity());
-            cartItemRepository.save(cartItem);
-            orderLineList.add(orderLine);
-            orderPrice.add(orderLine.getPrice());
+        List<OrderLine> orderLineList = cart.getCartItemSet().stream()
+                .map(cartItem -> {
+                    OrderLine orderLine = new OrderLine();
+                    orderLine.setProduct(cartItem.getProduct());
+                    orderLine.setOrder(order);
+                    orderLine.setPrice(cartItem.getProduct().getPrice());
+                    orderLine.setQuantity(cartItem.getQuantity());
 
-        });
+                    return orderLine;
+                })
+                .collect(Collectors.toList());
+
+        //trebuie sa facem o metoda care sa scada unitatile din stoc
+        orderLineList.forEach(orderLine ->
+                productService.decreaseStock(orderLine.getProduct().getId(), orderLine.getQuantity()));
+
+        BigDecimal totalPrice = orderLineList.stream()
+                .map(OrderLine::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        order.setTotalPrice(totalPrice);
+
+        order.setOrderLineList(orderLineList);
+        orderRepository.save(order);
+
+        cart.deleteAllItems();
+
         return orderLineList;
     }
 }
